014537277e4eafa317b7b430e97079ffb81ab98a
Tyler Cenac <tylerjcenac@gmail.com>
2022-11-30 12:58:48 -0600

    Added Swagger dependency and started creating project files

---
+++ RewardsService/src/main/java/org/example/service/RewardPointService.java
@@ -0,0 +1,13 @@
+package org.example.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RewardPointService {
+
+    public int calculateRewardPoints(int purchaseAmount){
+        return 0;
+    }
+
+
+}
