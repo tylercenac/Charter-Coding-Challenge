867a073c215ba05360792a2bc24ebc5e65a1585e
Tyler Cenac <tylerjcenac@gmail.com>
2022-11-30 13:10:19 -0600

    Added function to calculate reward points earned based on dollars spent

--- RewardsService/src/main/java/org/example/controller/RewardPointController.java
+++ RewardsService/src/main/java/org/example/controller/RewardPointController.java
@@ -1,14 +1,25 @@
 package org.example.controller;
 
+import org.example.service.RewardPointService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RewardPointController {
 
+    @Autowired
+    private RewardPointService rewardPointService;
+
     @GetMapping
     public String test(){
         return "testSuccessful";
+    }
+
+    @GetMapping("/rewardPoints/{purchaseAmount}")
+    public int getPointsFromPurchase(@PathVariable int purchaseAmount){
+        return rewardPointService.calculateRewardPoints(purchaseAmount);
     }
 
 }
--- RewardsService/src/main/java/org/example/service/RewardPointService.java
+++ RewardsService/src/main/java/org/example/service/RewardPointService.java
@@ -6,7 +6,17 @@ import org.springframework.stereotype.Service;
 public class RewardPointService {
 
     public int calculateRewardPoints(int purchaseAmount){
+
+        if(purchaseAmount > 100){
+           return ((purchaseAmount - 100) * 2) + 50;
+        }
+
+        if(purchaseAmount <= 100 && purchaseAmount > 50){
+            return purchaseAmount - 50;
+        }
+
         return 0;
+
     }
 
 
