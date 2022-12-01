86b1336bdaf987effae6e33e296b227bb79e980e
Tyler Cenac <tylerjcenac@gmail.com>
2022-12-01 09:56:31 -0600

    Improved comments and endpoint examples

--- RewardsService/src/main/java/org/example/controller/RewardPointController.java
+++ RewardsService/src/main/java/org/example/controller/RewardPointController.java
@@ -15,11 +15,11 @@ public class RewardPointController {
     private RewardPointService rewardPointService;
 
     @PostMapping("/purchase/{customerId}/{purchaseAmount}/{date}")
-    public String savePurchaseRecord(@PathVariable String customerId, @PathVariable int purchaseAmount, @PathVariable String date){
+    public String savePurchaseRecord(@ApiParam(value = "Example: 123") @PathVariable String customerId, @ApiParam(value = "Example: 120")  @PathVariable int purchaseAmount, @ApiParam(value = "Example: 11-30-2022")  @PathVariable String date){
         return rewardPointService.savePurchaseToDb(customerId, purchaseAmount, date);
     }
 
-    @GetMapping("/reward-points")
+    @GetMapping("/reward-points/total")
     public String getTotalRewardsPointsEarned(){
         return rewardPointService.getTotalRewardsPointsEarned();
     }
@@ -29,13 +29,13 @@ public class RewardPointController {
         return rewardPointService.getTotalRewardsPointsEarnedByMonth();
     }
 
-    @GetMapping("/reward-points/{customerId}")
-    public String getTotalRewardsPointsEarnedByCustomer(@ApiParam(value = "Example: 12345") @PathVariable String customerId){
+    @GetMapping("/reward-points/total/{customerId}")
+    public String getTotalRewardsPointsEarnedByCustomer(@ApiParam(value = "Example: 123") @PathVariable String customerId){
         return rewardPointService.getTotalRewardsPointsEarnedByCustomer(customerId);
     }
 
     @GetMapping("/reward-points/by-month/{customerId}")
-    public String getRewardsPointsEarnedByMonth(@PathVariable String customerId){
+    public String getRewardsPointsEarnedByMonth(@ApiParam(value = "Example: 123") @PathVariable String customerId){
         return rewardPointService.getRewardsPointsEarnedByCustomerByMonth(customerId);
     }
 
--- RewardsService/src/main/java/org/example/service/RewardPointService.java
+++ RewardsService/src/main/java/org/example/service/RewardPointService.java
@@ -25,7 +25,7 @@ public class RewardPointService {
         return String.format("Purchase for $%d by customer %s on %s saved! Customer has earned %d reward points for this purchase!", purchaseAmount, customerId, date, rewardPointsEarned);
     }
 
-    // Return the number of points a customer earns from a purchase
+    // Return the number of reward points a customer earns from a purchase
     public int calculateRewardPoints(int purchaseAmount){
 
         if(purchaseAmount > 100){
@@ -37,25 +37,27 @@ public class RewardPointService {
         }
 
         return 0;
-
     }
 
     // Returns the list of all customers found in the database with the total reward points they have each earned
     public String getTotalRewardsPointsEarned() {
 
-        List<PurchaseEntity> results = (List<PurchaseEntity>) rewardPointsRepository.findAll();
+        List<PurchaseEntity> purchaseEntities = (List<PurchaseEntity>) rewardPointsRepository.findAll();
         HashMap<String, Integer> rewardPointsMap = new HashMap<>();
 
-        for(PurchaseEntity result : results){
-            String customerId = result.getCustomerId();
+        for(PurchaseEntity purchaseEntity : purchaseEntities){
+            String customerId = purchaseEntity.getCustomerId();
 
             if(rewardPointsMap.containsKey(customerId)){
-                rewardPointsMap.put(customerId, rewardPointsMap.get(customerId) + result.getRewardPointsEarned());
+                // If rewardPointsMap already contains this customerId, add the reward points earned from this purchase entity to the existing total
+                rewardPointsMap.put(customerId, rewardPointsMap.get(customerId) + purchaseEntity.getRewardPointsEarned());
             }else{
-                rewardPointsMap.put(customerId, result.getRewardPointsEarned());
+                // Else add the new customerId to the rewardPointsMap with the reward points earned for just this purchase
+                rewardPointsMap.put(customerId, purchaseEntity.getRewardPointsEarned());
             }
         }
 
+        // Return results in a more readable format
         StringBuilder sb = new StringBuilder();
         for(String customerId : rewardPointsMap.keySet()){
             sb.append(String.format("Customer %s has earned a total of %d reward points.\n", customerId, rewardPointsMap.get(customerId)));
@@ -66,36 +68,42 @@ public class RewardPointService {
     // Returns the list of all customers found in the database with a breakdown of how many points they have earned in each month
     public String getTotalRewardsPointsEarnedByMonth() {
 
-        List<PurchaseEntity> results = (List<PurchaseEntity>) rewardPointsRepository.findAll();
+        List<PurchaseEntity> purchaseEntities = (List<PurchaseEntity>) rewardPointsRepository.findAll();
+
+        // customerMap will use customerId as the key and the value will be the list of all purchases made by that customer
         HashMap<String, List<PurchaseEntity>> customerMap = new HashMap<>();
 
         // For each purchase record
-        for(PurchaseEntity result : results){
-            String customerId = result.getCustomerId();
-            List<PurchaseEntity> purchaseEntities;
+        for(PurchaseEntity purchaseEntity : purchaseEntities){
+            String customerId = purchaseEntity.getCustomerId();
+            List<PurchaseEntity> purchaseEntitiesForThisCustomer;
 
             // Determine if customerMap already contains customer or not
-
-            //if customerMap contains customer, add purchaseEntity to list
             if(customerMap.containsKey(customerId)){
-                purchaseEntities = customerMap.get(customerId);
-
+                // If customerMap contains customer, grab the existing list of purchaseEntitiesForThisCustomer
+                purchaseEntitiesForThisCustomer = customerMap.get(customerId);
             }else{
-                purchaseEntities = new ArrayList<>();
+                // Else create a new a list that will store the purchaseEntitiesForThisCustomer
+                purchaseEntitiesForThisCustomer = new ArrayList<>();
 
             }
-            purchaseEntities.add(result);
-            customerMap.put(customerId, purchaseEntities);
+            purchaseEntitiesForThisCustomer.add(purchaseEntity);
+            customerMap.put(customerId, purchaseEntitiesForThisCustomer);
 
         }
 
+        // customerRewardPointsMap will use the customerId as a key and the value will be another map
+        // with its keys being the month and year of purchase (in mm-yyyy format) and its values being
+        // the number of reward points earned that month
         HashMap<String, HashMap<String, Integer>> customerRewardPointsMap = new HashMap<>();
 
+        // For each customer in the customer map, break down their reward points earned by month and store it in the customerRewardPointsMap
         for(String customerId : customerMap.keySet()){
             HashMap<String, Integer> rewardPointsByMonth = getRewardPointsEarnedByMonth(customerMap.get(customerId));
             customerRewardPointsMap.put(customerId, rewardPointsByMonth);
         }
 
+        // Return results in a more readable format
         StringBuilder sb = new StringBuilder();
         for(String customerId : customerRewardPointsMap.keySet()){
             sb.append(String.format("Customer %s reward points break-down by month:\n", customerId));
@@ -107,12 +115,10 @@ public class RewardPointService {
 
     // Given a customerId, returns the total number of reward points that customer has earned
     public String getTotalRewardsPointsEarnedByCustomer(String customerId) {
-        int totalRewardPointsEarned = 0;
         List<PurchaseEntity> results =  rewardPointsRepository.findByCustomerId(customerId);
 
-        for(PurchaseEntity result : results){
-            totalRewardPointsEarned += result.getRewardPointsEarned();
-        }
+        // Add all reward points earned from each purchase made by this customer
+        int totalRewardPointsEarned = results.stream().mapToInt(PurchaseEntity::getRewardPointsEarned).sum();
 
         return String.format("Customer %s has earned a total of %d reward points!", customerId, totalRewardPointsEarned);
     }
@@ -120,6 +126,10 @@ public class RewardPointService {
     // Given a customerId, returns the number of reward points earned by a specific customer separated by month
     public String getRewardsPointsEarnedByCustomerByMonth(String customerId) {
         List<PurchaseEntity> results =  rewardPointsRepository.findByCustomerId(customerId);
+
+        // Break down all reward points earned by month
+        // rewardPointsMap will have a key of month and year in mm-yyyy format
+        // and its value will be the number of reward points earned in that month
         HashMap<String, Integer> rewardPointsMap = getRewardPointsEarnedByMonth(results);
 
         return rewardPointsMap.toString();
@@ -131,13 +141,18 @@ public class RewardPointService {
         HashMap<String, Integer> rewardPointsMap = new HashMap<>();
 
         for(PurchaseEntity purchaseEntity : purchaseEntities){
+
+            // Convert the date from each purchase entity from mm-dd-yyyy to mm-yyyy
+            // as we only care about the month and year for this function
             int monthIndex = purchaseEntity.getDate().indexOf("-");
             int yearIndex = purchaseEntity.getDate().lastIndexOf("-");
             String monthAndYear = purchaseEntity.getDate().substring(0, monthIndex) + purchaseEntity.getDate().substring(yearIndex);
 
             if(rewardPointsMap.containsKey(monthAndYear)){
+                // If rewardPointsMap already has an entry for this month, add the reward points earned in this purchase to the existing total
                 rewardPointsMap.put(monthAndYear, rewardPointsMap.get(monthAndYear) + purchaseEntity.getRewardPointsEarned());
             }else{
+                // Else add this month to rewardPointsMap with the reward points earned from this purchase
                 rewardPointsMap.put(monthAndYear, purchaseEntity.getRewardPointsEarned());
             }
         }
